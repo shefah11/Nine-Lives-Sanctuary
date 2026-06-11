8acec3cb1c98c3a4b1cdc1b006b00f69b3621d9d
Mekyaa <alyaarasdi03@gmail.com>
2026-06-11 22:56:44 +0800

    Update AdoptionController.php

--- webapp/app/Http/Controllers/AdoptionController.php
+++ webapp/app/Http/Controllers/AdoptionController.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdoptionController extends Controller
+{
+    public function showApplyForm($cat_id)
+    {
+        return view('adoptions.apply');
+    }
+}
